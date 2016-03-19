bc4a4868a3fcb9fbe812283866d9d94cc7665980
kerwin <piaoyuankui@gmail.com>
2016-03-19 13:56:14 +0800

    Build

--- dist/index.js
+++ dist/index.js
@@ -49,6 +49,15 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 	var React = __webpack_require__(1);
 	var TextCell = __webpack_require__(158);
 	var CodeCell = __webpack_require__(209);
@@ -72,13 +81,28 @@
 	  }
 	}
 
-	var IPythonNotebook = function IPythonNotebook(props) {
-	  return React.createElement(
-	    'div',
-	    { className: 'ipynb' },
-	    props.data.cells.map(renderCell)
-	  );
-	};
+	var IPythonNotebook = function (_React$Component) {
+	  _inherits(IPythonNotebook, _React$Component);
+
+	  function IPythonNotebook() {
+	    _classCallCheck(this, IPythonNotebook);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(IPythonNotebook).apply(this, arguments));
+	  }
+
+	  _createClass(IPythonNotebook, [{
+	    key: 'render',
+	    value: function render() {
+	      return React.createElement(
+	        'div',
+	        { className: 'ipynb' },
+	        this.props.data.cells.map(renderCell)
+	      );
+	    }
+	  }]);
+
+	  return IPythonNotebook;
+	}(React.Component);
 
 	IPythonNotebook.propTypes = {
 	  data: React.PropTypes.object
